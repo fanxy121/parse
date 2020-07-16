ac5ff9727566651b00ab849acb1c1275c4e56a3f
videoparse-api <58383326+videoparse-api@users.noreply.github.com>
2020-07-16 22:53:14 +0800

    Update parse-safe.php

--- parse-safe.php
+++ parse-safe.php
@@ -10,7 +10,7 @@ function curlPost( $url = '', $data ) {
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url );
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1); //部分环境下，需要将参数值设为2，即：curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); //部分环境下，需要将参数值设为2，即：curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
